11da61d6500c3a80ac8f9fa2ce5e5cfae4c176d7
James Warmoth III <jwarmothiii@gmail.com>
2026-03-08 22:17:25 -0500

    question twelve

---
+++ src/main/java/dev/jameswarmothiii/assignment2/questions/twelve/DoubleLinkedSortedSeq.java
@@ -0,0 +1,218 @@
+package dev.jameswarmothiii.assignment2.questions.twelve;
+
+public class DoubleLinkedSortedSeq implements Cloneable {
+  private static final class DoubleNode {
+    private double dataValue;
+    private DoubleNode nextNode;
+
+    private DoubleNode(double initialDataValue, DoubleNode initialNextNode) {
+      dataValue = initialDataValue;
+      nextNode = initialNextNode;
+    }
+  }
+
+  private DoubleNode headNode;
+  private int manyItems;
+  private int currentIndex;
+
+  public DoubleLinkedSortedSeq() {
+    headNode = null;
+    manyItems = 0;
+    currentIndex = 0;
+  }
+
+  public static DoubleLinkedSortedSeq concatenation(
+      DoubleLinkedSortedSeq firstSequence, DoubleLinkedSortedSeq secondSequence) {
+    if (firstSequence == null || secondSequence == null) {
+      throw new NullPointerException("One of the sequences is null");
+    }
+
+    DoubleLinkedSortedSeq answer = new DoubleLinkedSortedSeq();
+
+    DoubleNode firstCursorNode = firstSequence.headNode;
+    DoubleNode secondCursorNode = secondSequence.headNode;
+
+    while (firstCursorNode != null && secondCursorNode != null) {
+      if (firstCursorNode.dataValue <= secondCursorNode.dataValue) {
+        answer.appendAtTail(firstCursorNode.dataValue);
+        firstCursorNode = firstCursorNode.nextNode;
+      } else {
+        answer.appendAtTail(secondCursorNode.dataValue);
+        secondCursorNode = secondCursorNode.nextNode;
+      }
+    }
+
+    while (firstCursorNode != null) {
+      answer.appendAtTail(firstCursorNode.dataValue);
+      firstCursorNode = firstCursorNode.nextNode;
+    }
+
+    while (secondCursorNode != null) {
+      answer.appendAtTail(secondCursorNode.dataValue);
+      secondCursorNode = secondCursorNode.nextNode;
+    }
+
+    answer.currentIndex = answer.manyItems;
+    return answer;
+  }
+
+  public void add(double elementValue) {
+    int insertionIndex = 0;
+    DoubleNode previousNode = null;
+    DoubleNode currentNode = headNode;
+
+    while (currentNode != null && currentNode.dataValue <= elementValue) {
+      previousNode = currentNode;
+      currentNode = currentNode.nextNode;
+      insertionIndex++;
+    }
+
+    DoubleNode newNode = new DoubleNode(elementValue, currentNode);
+    if (previousNode == null) {
+      headNode = newNode;
+    } else {
+      previousNode.nextNode = newNode;
+    }
+
+    manyItems++;
+    currentIndex = insertionIndex;
+  }
+
+  public void addAll(DoubleLinkedSortedSeq addend) {
+    if (addend == null) {
+      throw new NullPointerException("addend is null");
+    }
+
+    if (addend.manyItems == 0) {
+      return;
+    }
+
+    DoubleLinkedSortedSeq sourceSequence = (addend == this) ? addend.clone() : addend;
+
+    for (DoubleNode currentNode = sourceSequence.headNode;
+        currentNode != null;
+        currentNode = currentNode.nextNode) {
+      add(currentNode.dataValue);
+    }
+  }
+
+  public void advance() {
+    if (!isCurrent()) {
+      throw new IllegalStateException("No current element");
+    }
+
+    currentIndex++;
+  }
+
+  @Override
+  public DoubleLinkedSortedSeq clone() {
+    try {
+      DoubleLinkedSortedSeq copiedSequence = (DoubleLinkedSortedSeq) super.clone();
+      copiedSequence.headNode = copyNodeChain(headNode);
+      return copiedSequence;
+    } catch (CloneNotSupportedException cloneNotSupportedException) {
+      throw new AssertionError("Clone should be supported", cloneNotSupportedException);
+    }
+  }
+
+  public double getCurrent() {
+    if (!isCurrent()) {
+      throw new IllegalStateException("No current element");
+    }
+
+    return nodeAt(currentIndex).dataValue;
+  }
+
+  public boolean isCurrent() {
+    return currentIndex < manyItems;
+  }
+
+  public void removeCurrent() {
+    if (!isCurrent()) {
+      throw new IllegalStateException("No current element");
+    }
+
+    if (currentIndex == 0) {
+      headNode = headNode.nextNode;
+    } else {
+      DoubleNode previousNode = nodeAt(currentIndex - 1);
+      previousNode.nextNode = previousNode.nextNode.nextNode;
+    }
+
+    manyItems--;
+  }
+
+  public int size() {
+    return manyItems;
+  }
+
+  public void start() {
+    currentIndex = (manyItems == 0) ? manyItems : 0;
+  }
+
+  public double getElementAt(int indexValue) {
+    if (indexValue < 0 || indexValue >= manyItems) {
+      throw new IndexOutOfBoundsException("index: " + indexValue + ", size: " + manyItems);
+    }
+
+    return nodeAt(indexValue).dataValue;
+  }
+
+  public void setCurrentToIndex(int indexValue) {
+    if (indexValue < 0 || indexValue >= manyItems) {
+      throw new IndexOutOfBoundsException("index: " + indexValue + ", size: " + manyItems);
+    }
+
+    currentIndex = indexValue;
+  }
+
+  public void setCurrentToLast() {
+    if (manyItems == 0) {
+      currentIndex = manyItems;
+    } else {
+      currentIndex = manyItems - 1;
+    }
+  }
+
+  private static DoubleNode copyNodeChain(DoubleNode sourceHeadNode) {
+    if (sourceHeadNode == null) {
+      return null;
+    }
+
+    DoubleNode copiedHeadNode = new DoubleNode(sourceHeadNode.dataValue, null);
+    DoubleNode copiedTailNode = copiedHeadNode;
+
+    for (DoubleNode currentSourceNode = sourceHeadNode.nextNode;
+        currentSourceNode != null;
+        currentSourceNode = currentSourceNode.nextNode) {
+      copiedTailNode.nextNode = new DoubleNode(currentSourceNode.dataValue, null);
+      copiedTailNode = copiedTailNode.nextNode;
+    }
+
+    return copiedHeadNode;
+  }
+
+  private DoubleNode nodeAt(int indexValue) {
+    DoubleNode currentNode = headNode;
+
+    for (int currentPosition = 0; currentPosition < indexValue; currentPosition++) {
+      currentNode = currentNode.nextNode;
+    }
+
+    return currentNode;
+  }
+
+  private void appendAtTail(double elementValue) {
+    DoubleNode newNode = new DoubleNode(elementValue, null);
+
+    if (headNode == null) {
+      headNode = newNode;
+      manyItems = 1;
+      return;
+    }
+
+    DoubleNode tailNode = nodeAt(manyItems - 1);
+    tailNode.nextNode = newNode;
+    manyItems++;
+  }
+}
---
+++ src/test/java/dev/jameswarmothiii/assignment2/questions/twelve/DoubleLinkedSortedSeqTest.java
@@ -0,0 +1,290 @@
+package dev.jameswarmothiii.assignment2.questions.twelve;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import dev.jameswarmothiii.assignment2.questions.twelve.DoubleLinkedSortedSeq;
+import org.junit.jupiter.api.Test;
+
+class DoubleLinkedSortedSeqTest {
+
+  @Test
+  void defaultConstructorShouldCreateEmptySequence() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertEquals(0, sequence.size());
+    assertFalse(sequence.isCurrent());
+  }
+
+  @Test
+  void addShouldInsertIntoEmptySequence() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    sequence.add(5.0);
+
+    assertEquals(1, sequence.size());
+    assertTrue(sequence.isCurrent());
+    assertEquals(5.0, sequence.getCurrent(), 0.000001);
+    assertEquals(5.0, sequence.getElementAt(0), 0.000001);
+  }
+
+  @Test
+  void addShouldKeepElementsSortedWhenAddedOutOfOrder() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    sequence.add(5.0);
+    sequence.add(1.0);
+    sequence.add(3.0);
+    sequence.add(2.0);
+    sequence.add(4.0);
+
+    assertEquals(5, sequence.size());
+    assertEquals(1.0, sequence.getElementAt(0), 0.000001);
+    assertEquals(2.0, sequence.getElementAt(1), 0.000001);
+    assertEquals(3.0, sequence.getElementAt(2), 0.000001);
+    assertEquals(4.0, sequence.getElementAt(3), 0.000001);
+    assertEquals(5.0, sequence.getElementAt(4), 0.000001);
+  }
+
+  @Test
+  void addShouldAllowDuplicateValuesAndKeepSortedOrder() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    sequence.add(2.0);
+    sequence.add(2.0);
+    sequence.add(1.0);
+    sequence.add(2.0);
+
+    assertEquals(4, sequence.size());
+    assertEquals(1.0, sequence.getElementAt(0), 0.000001);
+    assertEquals(2.0, sequence.getElementAt(1), 0.000001);
+    assertEquals(2.0, sequence.getElementAt(2), 0.000001);
+    assertEquals(2.0, sequence.getElementAt(3), 0.000001);
+  }
+
+  @Test
+  void startShouldSetCurrentToFront() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(3.0);
+    sequence.add(1.0);
+    sequence.add(2.0);
+
+    sequence.setCurrentToLast();
+    sequence.start();
+
+    assertTrue(sequence.isCurrent());
+    assertEquals(1.0, sequence.getCurrent(), 0.000001);
+  }
+
+  @Test
+  void startShouldLeaveNoCurrentWhenSequenceIsEmpty() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    sequence.start();
+
+    assertFalse(sequence.isCurrent());
+  }
+
+  @Test
+  void advanceShouldMoveToNextElement() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(1.0);
+    sequence.add(2.0);
+    sequence.add(3.0);
+
+    sequence.start();
+    sequence.advance();
+
+    assertTrue(sequence.isCurrent());
+    assertEquals(2.0, sequence.getCurrent(), 0.000001);
+  }
+
+  @Test
+  void advanceShouldClearCurrentAtEnd() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(1.0);
+    sequence.add(2.0);
+
+    sequence.setCurrentToLast();
+    sequence.advance();
+
+    assertFalse(sequence.isCurrent());
+  }
+
+  @Test
+  void advanceShouldThrowWhenNoCurrent() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertThrows(IllegalStateException.class, sequence::advance);
+  }
+
+  @Test
+  void getCurrentShouldThrowWhenNoCurrent() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertThrows(IllegalStateException.class, sequence::getCurrent);
+  }
+
+  @Test
+  void removeCurrentShouldRemoveMiddleElement() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(3.0);
+    sequence.add(1.0);
+    sequence.add(2.0);
+
+    sequence.start();
+    sequence.advance();
+    sequence.removeCurrent();
+
+    assertEquals(2, sequence.size());
+    assertEquals(1.0, sequence.getElementAt(0), 0.000001);
+    assertEquals(3.0, sequence.getElementAt(1), 0.000001);
+    assertTrue(sequence.isCurrent());
+    assertEquals(3.0, sequence.getCurrent(), 0.000001);
+  }
+
+  @Test
+  void removeCurrentShouldClearCurrentWhenRemovingLastElement() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(1.0);
+    sequence.add(2.0);
+
+    sequence.setCurrentToLast();
+    sequence.removeCurrent();
+
+    assertEquals(1, sequence.size());
+    assertFalse(sequence.isCurrent());
+    assertEquals(1.0, sequence.getElementAt(0), 0.000001);
+  }
+
+  @Test
+  void removeCurrentShouldThrowWhenNoCurrent() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertThrows(IllegalStateException.class, sequence::removeCurrent);
+  }
+
+  @Test
+  void addAllShouldKeepSequenceSorted() {
+    DoubleLinkedSortedSeq firstSequence = new DoubleLinkedSortedSeq();
+    firstSequence.add(1.0);
+    firstSequence.add(4.0);
+    firstSequence.add(7.0);
+
+    DoubleLinkedSortedSeq secondSequence = new DoubleLinkedSortedSeq();
+    secondSequence.add(2.0);
+    secondSequence.add(3.0);
+    secondSequence.add(6.0);
+
+    firstSequence.start();
+    firstSequence.addAll(secondSequence);
+
+    assertEquals(6, firstSequence.size());
+    assertEquals(1.0, firstSequence.getElementAt(0), 0.000001);
+    assertEquals(2.0, firstSequence.getElementAt(1), 0.000001);
+    assertEquals(3.0, firstSequence.getElementAt(2), 0.000001);
+    assertEquals(4.0, firstSequence.getElementAt(3), 0.000001);
+    assertEquals(6.0, firstSequence.getElementAt(4), 0.000001);
+    assertEquals(7.0, firstSequence.getElementAt(5), 0.000001);
+  }
+
+  @Test
+  void addAllShouldThrowWhenAddendIsNull() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertThrows(NullPointerException.class, () -> sequence.addAll(null));
+  }
+
+  @Test
+  void concatenationShouldReturnSortedCombinedSequenceWithNoCurrent() {
+    DoubleLinkedSortedSeq firstSequence = new DoubleLinkedSortedSeq();
+    firstSequence.add(1.0);
+    firstSequence.add(3.0);
+    firstSequence.add(5.0);
+
+    DoubleLinkedSortedSeq secondSequence = new DoubleLinkedSortedSeq();
+    secondSequence.add(2.0);
+    secondSequence.add(4.0);
+    secondSequence.add(6.0);
+
+    DoubleLinkedSortedSeq combinedSequence =
+        DoubleLinkedSortedSeq.concatenation(firstSequence, secondSequence);
+
+    assertEquals(6, combinedSequence.size());
+    assertFalse(combinedSequence.isCurrent());
+    assertEquals(1.0, combinedSequence.getElementAt(0), 0.000001);
+    assertEquals(2.0, combinedSequence.getElementAt(1), 0.000001);
+    assertEquals(3.0, combinedSequence.getElementAt(2), 0.000001);
+    assertEquals(4.0, combinedSequence.getElementAt(3), 0.000001);
+    assertEquals(5.0, combinedSequence.getElementAt(4), 0.000001);
+    assertEquals(6.0, combinedSequence.getElementAt(5), 0.000001);
+  }
+
+  @Test
+  void concatenationShouldThrowWhenInputIsNull() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+
+    assertThrows(
+        NullPointerException.class, () -> DoubleLinkedSortedSeq.concatenation(sequence, null));
+    assertThrows(
+        NullPointerException.class, () -> DoubleLinkedSortedSeq.concatenation(null, sequence));
+  }
+
+  @Test
+  void cloneShouldCreateIndependentCopy() {
+    DoubleLinkedSortedSeq originalSequence = new DoubleLinkedSortedSeq();
+    originalSequence.add(3.0);
+    originalSequence.add(1.0);
+    originalSequence.add(2.0);
+    originalSequence.start();
+
+    DoubleLinkedSortedSeq clonedSequence = originalSequence.clone();
+
+    assertNotSame(originalSequence, clonedSequence);
+    assertEquals(originalSequence.size(), clonedSequence.size());
+    assertEquals(1.0, clonedSequence.getElementAt(0), 0.000001);
+    assertEquals(2.0, clonedSequence.getElementAt(1), 0.000001);
+    assertEquals(3.0, clonedSequence.getElementAt(2), 0.000001);
+    assertTrue(clonedSequence.isCurrent());
+    assertEquals(1.0, clonedSequence.getCurrent(), 0.000001);
+
+    clonedSequence.add(4.0);
+
+    assertEquals(3, originalSequence.size());
+    assertEquals(4, clonedSequence.size());
+  }
+
+  @Test
+  void getElementAtShouldThrowForInvalidIndex() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(1.0);
+
+    assertThrows(IndexOutOfBoundsException.class, () -> sequence.getElementAt(-1));
+    assertThrows(IndexOutOfBoundsException.class, () -> sequence.getElementAt(1));
+  }
+
+  @Test
+  void setCurrentToIndexShouldSetCurrent() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(30.0);
+    sequence.add(10.0);
+    sequence.add(20.0);
+
+    sequence.setCurrentToIndex(1);
+
+    assertTrue(sequence.isCurrent());
+    assertEquals(20.0, sequence.getCurrent(), 0.000001);
+  }
+
+  @Test
+  void setCurrentToIndexShouldThrowForInvalidIndex() {
+    DoubleLinkedSortedSeq sequence = new DoubleLinkedSortedSeq();
+    sequence.add(1.0);
+
+    assertThrows(IndexOutOfBoundsException.class, () -> sequence.setCurrentToIndex(-1));
+    assertThrows(IndexOutOfBoundsException.class, () -> sequence.setCurrentToIndex(2));
+  }
+}
